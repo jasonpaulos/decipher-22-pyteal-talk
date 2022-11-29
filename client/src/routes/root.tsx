94701d2cae4da1fcf0a6a90658a01acd57bbec3c
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-11-29 14:02:13 +0400

    Update header, auto refresh account balance if 0

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Outlet, useLoaderData } from "react-router-dom";
 import algosdk from 'algosdk';
 import { getAccount, getAlgod } from './settings';
@@ -21,16 +21,51 @@ export async function loader(): Promise<RootData> {
   };
 }
 
+function addressPreview(addr: string): string {
+  return `${addr.substring(0,5)}...${addr.substring(addr.length-5, addr.length)}`
+}
+
 export function Root() {
-  const { account, balance } = useLoaderData() as RootData;
+  const { account, balance: initialBalance } = useLoaderData() as RootData;
+  const [balance, setBalance] = useState(initialBalance);
+
+  useEffect(() => {
+    const delay = 5000;
+
+    async function refreshBalance() {
+      try {
+        if (balance) {
+          return;
+        }
+
+        const algod = getAlgod();
+        const account = getAccount();
+
+        const info = await algod.accountInformation(account.addr).exclude("all").do();
+        const newBalance = info["amount"] as number;
+
+        if (newBalance) {
+          setBalance(newBalance);
+        } else {
+          setTimeout(refreshBalance, delay);
+        }
+      } catch (err) {
+        console.error("Cannot refresh account balance:", err);
+      }
+    }
+
+    setTimeout(refreshBalance, delay);
+  });
+
   return (
     <div className="root">
       <nav className="root-header">
         <span>Algorand Polling Demo</span>
         <Link to="/">Home</Link>
         <Link to="/settings">Settings</Link>
+        <a href="https://github.com/jasonpaulos/decipher-22-pyteal-talk">GitHub</a>
         <div></div>
-        <span className="account">Account: {account.addr}</span>
+        <span>Account: {addressPreview(account.addr)}</span>
       </nav>
       {!balance && <div className="dispenser-request">
             <p>
